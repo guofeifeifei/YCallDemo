a4cdbea51a79775d6fcaa55524e8b0ddd172bafb
郭飞飞 <myEmail@example.com>
2018-01-26 13:04:06 +0800

    Initial Commit

---
+++ YCallShiPin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCallShiPin
+//
+//  Created by ZZCN77 on 2018/1/26.
+//  Copyright © 2018年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCallShiPin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YCallShiPin
+//
+//  Created by ZZCN77 on 2018/1/26.
+//  Copyright © 2018年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
